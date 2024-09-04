b8a0ccc5b23a2318357dc232c26cc7cf1b729244
Valentin Schiaffino <valenschg@gmail.com>
2024-09-04 19:49:06 -0300

    feat: basic tab routing

---
+++ app/contador.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Contador = () => {
+  return (
+    <View>
+      <Text>Contador</Text>
+    </View>
+  )
+}
+
+export default Contador
---
+++ app/tarjetas.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Tarjetas = () => {
+  return (
+    <View>
+      <Text>Tarjetas</Text>
+    </View>
+  )
+}
+
+export default Tarjetas
