3149ed86150bda67c1024f5c2a6fb1e12be88204
Valentin Schiaffino <valenschg@gmail.com>
2024-09-04 20:33:45 -0300

    feat: basic perfil route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack, Tabs } from 'expo-router'
 
 export default function RootLayout() {
-  return <Tabs />
+  return (
+    <Tabs>
+      <Tabs.Screen name='contador' />
+      <Tabs.Screen name='perfil' />
+      <Tabs.Screen name='tarjetas' />
+    </Tabs>
+  )
 }
--- app/perfil.tsx
+++ app/perfil.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Modal, TextInput } from 'react-native'
+import React, { useState } from 'react'
 
 const Perfil = () => {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [nameToChange, setNameToChange] = useState('')
+  const [name, setName] = useState('Valentín Schiaffino')
   return (
     <View>
-      <Text>Perfil</Text>
+      <Modal visible={modalVisible} transparent={true}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <View
+            style={{
+              width: 200,
+              height: 100,
+              borderRadius: 20,
+              backgroundColor: 'red',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <TextInput
+              onChangeText={(text) => setNameToChange(text)}
+              value={nameToChange}
+            />
+            <Button
+              title='Guardar nombre'
+              onPress={() => {
+                setName(nameToChange)
+                setNameToChange('')
+                setModalVisible(false)
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+      <Text>Hola {name}</Text>
+      <Button title='Cambiar nombre' onPress={() => setModalVisible(true)} />
     </View>
   )
 }
